e9d228c0ac3ba226cca50572582d180616e7bbe1
konyadonat <konya.donat@gmail.com>
2022-11-24 10:18:11 +0100

    footer, header, main blade files created

---
+++ ami-journal/resources/views/layouts/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="text-center">
+            <div class="container text-muted py-4 py-lg-5">
+                <p class="mb-0" style="color: rgb(111,121,129);"><br>This website is developed by Kiss Barnabás, Kónya Donát, Szegedi Tamás and Bagoly Gábor.<br>© 2010-2022 Eszterházy Károly University. All rights reserved.<br><br></p>
+            </div>
+        </footer>
---
+++ ami-journal/resources/views/layouts/main.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+        <title>Ami</title>
+        <link rel="stylesheet" href="/css/bootstrap.min.css">
+        <link rel="stylesheet" href="/css/styles.min.css">
+        <link rel="stylesheet" href="/css/aos.min.css">
+    </head>
+    <body>
+        @include('layouts.header')
+
+        <main class="container">
+            @yield('content')
+        </main>
+        
+        @include('layouts.footer')
+
+        @stack('scripts')
+        <script src="/js/bootstrap.js"></script>
+        <script src="/js/script.min.js"></script>
+        <script src="/js/aos.min.min.js"></script>
+    </body>
+</html>
