527441e52f26c4ebf33e3a1490865ceaef0d1638
OOHQ3E <gaboly15@gmail.com>
2023-04-26 09:28:02 +0200

    Feat: Login button now routes to login panel

--- ami-journal/resources/views/layouts/header.blade.php
+++ ami-journal/resources/views/layouts/header.blade.php
@@ -1,11 +1,11 @@
 <div class="container" data-aos="fade" data-aos-duration="1000" data-aos-once="true">
     <header>
         <div class="row">
-            <div class="col-xl-3 d-xl-flex justify-content-xl-start align-items-xl-center"><img class="img-fluid" src="/img/summa.jpg"></div>
+            <div class="col-xl-3 d-xl-flex justify-content-xl-start align-items-xl-center"><img class="img-fluid" src="/img/summa.jpg" alt="Image of Summa"></div>
             <div class="col offset-xl-0 d-xl-flex align-items-xl-center">
                 <h1 class="text-center" style="color: rgb(41,47,61);font-weight: bold;padding-left: 4vw;">ANNALES MATHEMATICAE<br><span style="color: rgb(186, 186, 186);">ET</span> INFORMATICAE<br></h1>
             </div>
-            <div class="col-xl-2" style="text-align: right;"><a class="btn btn-primary" role="button" style="margin-top: 2vw;--bs-primary: #004863;--bs-primary-rgb: 0,72,99;background: rgb(0,72,99);">Login</a></div>
+            <div class="col-xl-2" style="text-align: right;"><a href="{{ asset('login') }}" class="btn btn-primary" role="button" style="margin-top: 2vw;--bs-primary: #004863;--bs-primary-rgb: 0,72,99;background: rgb(0,72,99);">Login</a></div>
         </div>
     </header>
 </div>
