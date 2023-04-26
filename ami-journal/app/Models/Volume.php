6d28081ab7125f32810bc3a3658326badf84abd8
vamosakos <vammarton@gmail.com>
2023-04-26 03:18:17 +0200

    added volume model

---
+++ ami-journal/app/Models/Volume.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Volume extends Model 
+{
+
+    protected $table = 'volumes';
+    public $timestamps = true;
+
+}
