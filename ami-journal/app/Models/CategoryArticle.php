da75cf860ed63c65a678e5a9d4a21611c98603d7
rcfr84 <valcsi20010923@gmail.com>
2023-05-02 18:42:46 +0200

    added category article model

---
+++ ami-journal/app/Models/CategoryArticle.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CategoryArticle extends Model 
+{
+
+    protected $table = 'category_article';
+    public $timestamps = false;
+
+}
