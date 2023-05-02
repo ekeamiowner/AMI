df945e41938cbcb29905caba1a88ece44e37b3a4
vamosakos <vamos.akos1337@gmail.com>
2023-05-03 01:26:03 +0200

    modified categories table

--- ami-journal/database/migrations/2023_04_26_021953_create_categories_table.php
+++ ami-journal/database/migrations/2023_04_26_021953_create_categories_table.php
@@ -8,9 +8,8 @@ class CreateCategoriesTable extends Migration {
 	public function up()
 	{
 		Schema::create('categories', function(Blueprint $table) {
-			$table->id();
+			$table->integer('id', true)->unsigned();
 			$table->string('name');
-			$table->timestamps();
 		});
 	}
 
