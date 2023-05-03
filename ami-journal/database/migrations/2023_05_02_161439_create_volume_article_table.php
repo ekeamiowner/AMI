c6b92d2be7cb280ec125a0c3fbfe1b33c4983c8e
vamosakos <vammarton@gmail.com>
2023-05-03 02:27:42 +0200

    modified volume article table

---
+++ ami-journal/database/migrations/2023_05_02_161439_create_volume_article_table.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateVolumeArticleTable extends Migration {
+
+	public function up()
+	{
+		Schema::create('volume_article', function(Blueprint $table) {
+			$table->integer('volume_id')->unsigned();
+			$table->string('article_id');
+			$table->integer('from_page')->unsigned();
+			$table->integer('to_page')->unsigned();
+		});
+	}
+
+	public function down()
+	{
+		Schema::drop('volume_article');
+	}
+}
