336bc5a383cb7e077ec06b9f178619888b4d75d5
vamosakos <vammarton@gmail.com>
2023-05-03 02:29:13 +0200

    modified category article table

---
+++ ami-journal/database/migrations/2023_05_02_161439_create_category_article_table.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateCategoryArticleTable extends Migration {
+
+	public function up()
+	{
+		Schema::create('category_article', function(Blueprint $table) {
+			$table->integer('category_id')->unsigned();
+			$table->string('article_id');
+		});
+	}
+
+	public function down()
+	{
+		Schema::drop('category_article');
+	}
+}
