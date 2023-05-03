86c45e64febcc7ccb7c25cdf9ec6b48a217f975c
vamosakos <vammarton@gmail.com>
2023-05-03 02:05:18 +0200

    modified articles table

--- ami-journal/database/migrations/2023_04_25_233818_create_articles_table.php
+++ ami-journal/database/migrations/2023_04_25_233818_create_articles_table.php
@@ -8,22 +8,20 @@ class CreateArticlesTable extends Migration {
 	public function up()
 	{
 		Schema::create('articles', function(Blueprint $table) {
-			$table->uuid('id');
-			$table->uuid('user_id')->nullable()->default(null);
-			$table->uuid('editor_id')->nullable()->default(null);
+			$table->string('id')->primary();
+			$table->string('user_id');
+			$table->string('editor_id')->nullable()->default(null);
 			$table->string('title');
 			$table->text('abstract');
-			$table->tinyInteger('state')->default(0);
+			$table->enum('state', array('SUBMITTED', 'UNDER_REVIEW', 'ACCEPTED', 'REJECTED'));
 			$table->integer('page_count')->nullable()->default(null);
 			$table->text('note')->nullable()->default(null);
 			$table->string('language')->default('en');
 			$table->string('doi')->nullable()->default(null);
 			$table->text('source')->nullable()->default(null);
-			$table->integer('type_id')->nullable();
-			$table->integer('volume_id')->default(0);
+			$table->integer('type_id')->unsigned();
 			$table->string('latex_path')->nullable()->default(null);
 			$table->timestamp('deleted_at')->nullable()->default(null);
-			$table->timestamps();
 		});
 	}
 
