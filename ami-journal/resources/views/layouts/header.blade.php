a3632f9210a9d4815affe0be0755ed6fd51039fc
IamDani13 <gomoridaniel13@gmail.com>
2023-11-16 12:09:41 +0100

    Navigációs bár javítása
    A navigációs bár kitölti a képernyő tetejét.

--- ami-journal/resources/views/layouts/header.blade.php
+++ ami-journal/resources/views/layouts/header.blade.php
@@ -17,18 +17,22 @@
         background-color: #e3ff54;
         color: black;
     }
-</style>
 
-<div class="container" data-aos="fade" data-aos-duration="1000" data-aos-once="true">
-    <header>
-    <div class="nav">
-        <a>home</a>
-        <a>browse</a>
-        <a>submissions</a>
+    .nav a:visited{
+        color: black;
+    }
+</style>
+<div class="nav">
+        <a href="http://127.0.0.1:8000/">Home</a>
+        <a>Browse</a>
+        <a>Submissions</a>
         <a>About</a>
         <input style="height: 30px; margin-top: auto; margin-bottom: auto; margin-left: auto;"></input>
         <a href="{{ asset('login') }}" class="btn btn-primary" role="button" style="margin-left: auto; margin-right: 150px; --bs-primary: #004863;--bs-primary-rgb: 0,72,99;background: rgb(0,72,99);">Login</a>
-    </div>
+</div>
+<div class="container" data-aos="fade" data-aos-duration="1000" data-aos-once="true">
+    <header>
+    
         <div class="row">
             <div class="col-xl-3 d-xl-flex justify-content-xl-start align-items-xl-center"><img class="img-fluid" src="/img/summa.jpg" alt="Image of Summa"></div>
             <div class="col offset-xl-0 d-xl-flex align-items-xl-center">
