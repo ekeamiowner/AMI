8042f4fd70c1d8fd4a7c10451548f743ee8b733a
Bmate2 <bmate2002.01@gmail.com>
2023-11-23 13:25:53 +0100

    Guest or auth routes

--- ami-journal/routes/web.php
+++ ami-journal/routes/web.php
@@ -25,13 +25,18 @@ Route::get('/login',function () {
 Route::get('/admin', function() {
     return view('adminpanel.adminpanel');
 });
-Route::get('/register', [AuthController::class, 'register'])->name('register');
-Route::post('/register', [AuthController::class, 'registerPost'])->name('register');
-Route::get('/login', [AuthController::class, 'login'])->name('login');
-Route::post('/login', [AuthController::class, 'loginPost'])->name('login');
-Route::get('/home', [HomeController::class, 'index']);
-Route::delete('/logout', [AuthController::class, 'logout'])->name('logout');
 
+Route::group(['middleware' => 'guest'], function () {
+    Route::get('/register', [AuthController::class, 'register'])->name('register');
+    Route::post('/register', [AuthController::class, 'registerPost'])->name('register');
+    Route::get('/login', [AuthController::class, 'login'])->name('login');
+    Route::post('/login', [AuthController::class, 'loginPost'])->name('login');
+});
+ 
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/home', [HomeController::class, 'index']);
+    Route::delete('/logout', [AuthController::class, 'logout'])->name('logout');
+});
 Route::get('/about', function() {
     return view('pages.about');
 });
