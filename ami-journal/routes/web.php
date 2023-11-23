e9b5ed21704ae8a437fca42d6f70cd70dc5eb2fb
Bmate2 <bmate2002.01@gmail.com>
2023-11-23 12:20:31 +0100

    HomeController index function, home Blade, home route

--- ami-journal/routes/web.php
+++ ami-journal/routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -27,3 +28,5 @@ Route::get('/register', [AuthController::class, 'register'])->name('register');
 Route::post('/register', [AuthController::class, 'registerPost'])->name('register');
 Route::get('/login', [AuthController::class, 'login'])->name('login');
 Route::post('/login', [AuthController::class, 'loginPost'])->name('login');
+
+Route::get('/home', [HomeController::class, 'index']);
