2a3dcb5a84acac4ca32a4584ae41000b4fdd99ff
Buda Dávid <davdbudababa@gmail.com>
2023-12-05 23:23:23 +0100

    Type.php Elkészítése
    Type.php Modell Frissítése

--- ami-journal/app/Models/Type.php
+++ ami-journal/app/Models/Type.php
@@ -2,12 +2,27 @@
 
 namespace App\Models;
 
+use App\Traits\ApiResource;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
-class Type extends Model 
+/**
+ * Class Type
+ * @package App\Models
+ *
+ * @property int id
+ * @property string name
+ * @property boolean active
+ */
+class Type extends Model
 {
+    use HasFactory, ApiResource;
 
-    protected $table = 'types';
-    public $timestamps = true;
+    protected $fillable = ['name', 'active'];
+
+    public function article(): BelongsTo{
+        return $this->belongsTo(Article::class);
+    }
 
 }
