ee6e38aeae96c047ffd84e194dc70a91432e3a0b
Buda Dávid <davdbudababa@gmail.com>
2023-12-06 18:02:16 +0100

    Volume.php Elkészítése
    Volume.php Modell Elkészítése

--- ami-journal/app/Models/Volume.php
+++ ami-journal/app/Models/Volume.php
@@ -2,12 +2,27 @@
 
 namespace App\Models;
 
+use App\Traits\ApiResource;
+use DateTime;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
-class Volume extends Model 
+
+class Volume extends Model
 {
+    use HasFactory, ApiResource;
 
-    protected $table = 'volumes';
-    public $timestamps = true;
+    protected $fillable = ['title', 'description', 'release_year'];
 
+    public function getTitleAttribute(): string
+    {
+        return $this->title ?? "#$this->id"; // Változtatás a mezőnévben, az angol változat alapján
+    }
+
+    public function articles(): HasMany
+    {
+        return $this->hasMany(Article::class);
+    }
 }
