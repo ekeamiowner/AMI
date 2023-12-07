ab06622452086a6774315a9355f5ddc2e5055382
Csasziuser <145013640+Csasziuser@users.noreply.github.com>
2023-12-07 12:39:15 +0100

    fixed navbar issue
    activate navbar submissions button conflict

--- ami-journal/resources/views/layouts/navbar.blade.php
+++ ami-journal/resources/views/layouts/navbar.blade.php
@@ -1,19 +1,9 @@
 <link rel="stylesheet" href="/css/bootstrap.min.css">
 <link rel="stylesheet" href="/css/navbar.css">
 <div class="nav">
-        <a href="http://127.0.0.1:8000/">Home</a>
-        <a>Browse</a>
-        <a href="http://127.0.0.1:8000/submissions">Submissions</a>
-        <a href="http://127.0.0.1:8000/about">About</a>
-        <input style="height: 30px; margin-top: auto; margin-bottom: auto; margin-left: auto;"></input>
-        <a href="{{ asset('login') }}" style=" margin-left: auto;">Login</a>
-</div>
-<link rel="stylesheet" href="/css/bootstrap.min.css">
-<link rel="stylesheet" href="/css/navbar.css">
-<div class="nav">
     <a href="http://127.0.0.1:8000/">Home</a>
     <a>Browse</a>
-    <a>Submissions</a>
+    <a href="http://127.0.0.1:8000/submissions">Submissions</a>
     <a href="http://127.0.0.1:8000/about">About</a>
     <input style="height: 30px; margin-top: auto; margin-bottom: auto; margin-left: auto;"></input>
 
