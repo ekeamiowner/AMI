65c9f825c4ae6c3a5080045ff74b81a7170d2609
GubaJozsef <jozsefguba99@gmail.com>
2023-12-07 14:36:11 +0100

    create articleindex.blade.php

---
+++ ami-journal/resources/views/pages/articleindex.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.main')
+
+@section('content')
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/submissionform.css">
+    <title>Articles</title>
+</head>
+<body>
+    
+</body>
+</html>
+@endsection
