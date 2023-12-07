4b7ba2e4ef8152d0b3bbf68af565581167c74e6b
Vamos Akos <vamos.akos1337@gmail.com>
2023-12-07 14:45:27 +0100

    revert to previous User model

--- ami-journal/app/Models/User.php
+++ ami-journal/app/Models/User.php
@@ -2,33 +2,36 @@
 
 namespace App\Models;
 
-use App\Traits\ApiKey;
-use App\Traits\ApiResource;
-use App\Traits\UUID;
-use DateTime;
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Silber\Bouncer\Database\HasRolesAndAbilities;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable, ApiResource, UUID, HasRolesAndAbilities, ApiKey;
+    use HasApiTokens, HasFactory, Notifiable;
 
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name',
+        'email',
+        'password'
     ];
 
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
     protected $hidden = [
-        'password', 'remember_token', 'api_key'
+        'password',
+        'remember_token'
     ];
 
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
-
-    public function reviews() : HasMany {
-        return $this->hasMany(Revision::class)->withPivot(['state', 'content']);
-    }
+    public $timestamps = false;
 }
