832bc1b0ac7bb0682ab23f8f9d8848e98ea6e16e
Csasziuser <145013640+Csasziuser@users.noreply.github.com>
2023-12-11 18:33:33 +0100

    Edited the frontend of the Article page
    Made the separation and "painting" for the articles, which has been listed on the page

--- ami-journal/resources/views/pages/articleindex.blade.php
+++ ami-journal/resources/views/pages/articleindex.blade.php
@@ -9,22 +9,65 @@
     <link rel="stylesheet" href="/css/submissionform.css">
     <title>Articles</title>
 </head>
+<style>
+    .article-container 
+    {
+    margin-bottom: 10px;
+    padding: 10px;
+    border: 1px solid #ccc;
+    }
+
+    .header-section 
+    {
+        background-color: #f2f2f2;
+        padding: 10px;
+    }
+
+    .abstract-section 
+    {
+        background-color: #e3ff54;
+        padding: 10px;
+    }
+
+    .language 
+    {
+        font-weight: bold;
+    }
+
+    .author-reviewer 
+    {
+    margin-top: 5px;
+    }
+
+    .author,
+    .reviewer 
+    {
+    display: inline-block;
+    margin-right: 10px;
+    }
+</style>
 <body>
     <h1>Articles</h1>
     <form action="{{ route('pages.articleindex') }}" method="GET">
         <input type="text" name="search" placeholder="Search articles">
         <button type="submit">Search</button>
     </form>
-
+    <br></br>
     <ul>
         @foreach($articles as $article)
-            <li>
-                <strong>Title:</strong> {{ $article->title }}<br>
-                <strong>Author:</strong> {{ optional($article->user)->name }}<br>
-                <strong>Editor:</strong> {{ optional($article->editor)->name }}<br>
-                <strong>Abstract:</strong> {{ $article->abstract }}<br>
-                <strong>Language:</strong> {{ $article->language }}<br>
-            </li>
+            <div class="article-container">
+                <div class="header-section">
+                    <h3> {{ $article->title }}</h3>
+                    <div class="author-reviewer">
+                        <span class="author">Author: {{ optional($article->user)->name }}</span>
+                        <span class="editor">Editor: {{ optional($article->editor)->name }}</span>
+                    </div>
+                </div>
+                <div class="abstract-section">
+                    <div class="abstract-content">{{ $article->abstract }}</div>
+                    <div class="language">{{ $article->language }}</div>
+                </div>
+            </div>
             <hr>
         @endforeach
     </ul>
