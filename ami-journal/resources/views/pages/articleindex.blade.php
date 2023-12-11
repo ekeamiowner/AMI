bdda13d469331b7f1ed7184db32f3d1e4302403e
Csasziuser <145013640+Csasziuser@users.noreply.github.com>
2023-12-11 19:00:03 +0100

    Implemented pagination and updated styling for article listing page.

--- ami-journal/resources/views/pages/articleindex.blade.php
+++ ami-journal/resources/views/pages/articleindex.blade.php
@@ -8,52 +8,56 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/css/submissionform.css">
     <title>Articles</title>
+    <style>
+        .article-container 
+        {
+        border: 1px solid #ccc;
+        border-radius: 20px;
+        }
+
+        .header-section 
+        {
+            background-color: #f2f2f2;
+            padding: 10px;
+            border-radius: 20px 20px 0 0;
+        }
+
+        .abstract-section 
+        {
+            background-color: #e3ff54;
+            padding: 10px;
+            border-radius: 0 0 20px 20px;
+        }
+
+        .language 
+        {
+            font-weight: bold;
+        }
+
+        .author-reviewer 
+        {
+        margin-top: 5px;
+        }
+
+        .author,
+        .reviewer 
+        {
+        display: inline-block;
+        margin-right: 10px;
+        }
+    </style>
 </head>
-<style>
-    .article-container 
-    {
-    margin-bottom: 10px;
-    padding: 10px;
-    border: 1px solid #ccc;
-    }
-
-    .header-section 
-    {
-        background-color: #f2f2f2;
-        padding: 10px;
-    }
-
-    .abstract-section 
-    {
-        background-color: #e3ff54;
-        padding: 10px;
-    }
-
-    .language 
-    {
-        font-weight: bold;
-    }
-
-    .author-reviewer 
-    {
-    margin-top: 5px;
-    }
-
-    .author,
-    .reviewer 
-    {
-    display: inline-block;
-    margin-right: 10px;
-    }
-</style>
 <body>
     <h1>Articles</h1>
-    <form action="{{ route('pages.articleindex') }}" method="GET">
-        <input type="text" name="search" placeholder="Search articles">
-        <button type="submit">Search</button>
+    <form action="{{ route('pages.articleindex') }}" method="GET" class="form-inline">
+        <div class="input-group">
+            <input type="text" name="search" class="form-control" placeholder="Search articles" value="{{ $search }}">
+            <div class="input-group-append">
+                <button type="submit" class="btn btn-primary">Search</button>
+            </div>
+        </div>
     </form>
     <br></br>
-    <ul>
         @foreach($articles as $article)
             <div class="article-container">
                 <div class="header-section">
@@ -70,8 +74,6 @@
             </div>
             <hr>
         @endforeach
-    </ul>
-
     <div class="pagination">
         {{ $articles->links() }}
     </div>
