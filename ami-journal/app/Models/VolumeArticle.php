bd0054b5241619a8ba7bfee2969e857fba9639d8
Buda Dávid <114563545+Budababa@users.noreply.github.com>
2023-12-14 12:13:59 +0100

    VolumeArticle.php Megírása
    VolumeArticle.php modell megírása

--- ami-journal/app/Models/VolumeArticle.php
+++ ami-journal/app/Models/VolumeArticle.php
@@ -3,11 +3,18 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
-class VolumeArticle extends Model 
+class VolumeArticle extends Model
 {
-
     protected $table = 'volume_article';
-    public $timestamps = false;
 
+    public $timestamps = true; // Ha szükséges
+
+    public function volumes(): BelongsToMany
+    {
+        return $this->belongsToMany(Volume::class, 'volume_article', 'article_id', 'volume_id')
+            ->withPivot(['from_page', 'to_page'])
+            ->withTimestamps();
+    }
 }
