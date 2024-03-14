ac86d168ba2b0259ef28651423bc915f3b2ba865
vamosmarton <vammarton@gmail.com>
2024-03-14 09:27:32 +0100

    changed header width

--- ami-journal/resources/views/layouts/header.blade.php
+++ ami-journal/resources/views/layouts/header.blade.php
@@ -1,12 +1,10 @@
-<div class="container" data-aos="fade" data-aos-duration="1000" data-aos-once="true">
+<div class="container-fluid" data-aos="fade" data-aos-duration="1000" data-aos-once="true">
     <header>
-    
         <div class="row">
             <div class="col-xl-3 d-xl-flex justify-content-xl-start align-items-xl-center"><img class="img-fluid" src="/img/summa.jpg" alt="Image of Summa"></div>
             <div class="col offset-xl-0 d-xl-flex align-items-xl-center">
                 <h1 class="text-center" style="color: rgb(41,47,61);font-weight: bold;padding-left: 4vw;">ANNALES MATHEMATICAE<br><span style="color: rgb(186, 186, 186);">ET</span> INFORMATICAE<br></h1>
             </div>
-            
         </div>
     </header>
 </div>
