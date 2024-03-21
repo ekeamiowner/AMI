cd737b324c684d78a4cfc14aa044a87ae6d8dd06
MizereRobin <mizererobin@gmail.com>
2024-03-21 12:33:17 +0100

    Navbar_Fixed_240321
    - Navbar fixed to top
    - Logout fixed

--- ami-journal/resources/views/layouts/navbar.blade.php
+++ ami-journal/resources/views/layouts/navbar.blade.php
@@ -1,5 +1,5 @@
 <link rel="stylesheet" href="/css/navbar.css">
-<div class="nav" id="navbar">
+<div class="nav sticky-top" id="navbar">
     <table id="navbar_table">
         <tr>
             <td style="border-right: 2px solid black;"><a href="{{ route('welcome.index') }}">Home</a></td>
@@ -26,30 +26,30 @@
      -- END SEARCHBAR -->
 
     @guest
-        <a href="{{ route('login') }}" style="margin-left: auto;">
+        <a href="{{ route('login') }}" style="margin-left: auto; padding-right:25px;">
             <!-- Login icon -->
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-in-right" viewBox="0 0 16 16">
+            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-box-arrow-in-right" viewBox="0 0 16 16">
                 <path fill-rule="evenodd" d="M6 3.5a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-2a.5.5 0 0 0-1 0v2A1.5 1.5 0 0 0 6.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-8A1.5 1.5 0 0 0 5 3.5v2a.5.5 0 0 0 1 0z"/>
                 <path fill-rule="evenodd" d="M11.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H1.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708z"/>
             </svg>
             <!-- END ICON -->
         </a>
     @else
-        <a href="{{ route('logout') }}"
+        <a href="{{ route('logout') }} "
            onclick="event.preventDefault(); document.getElementById('logout-form').submit();"
-           style="margin-left: auto;">
-            Logout
+           style="margin-left: auto; padding-right:25px;">
+            <!-- Logout icon -->
+            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-box-arrow-left" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M6 12.5a.5.5 0 0 0 .5.5h8a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5h-8a.5.5 0 0 0-.5.5v2a.5.5 0 0 1-1 0v-2A1.5 1.5 0 0 1 6.5 2h8A1.5 1.5 0 0 1 16 3.5v9a1.5 1.5 0 0 1-1.5 1.5h-8A1.5 1.5 0 0 1 5 12.5v-2a.5.5 0 0 1 1 0z"/>
+                <path fill-rule="evenodd" d="M.146 8.354a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L1.707 7.5H10.5a.5.5 0 0 1 0 1H1.707l2.147 2.146a.5.5 0 0 1-.708.708z"/>
+            </svg>
+            <!-- END ICON -->
         </a>
         <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
             @csrf
             @method('DELETE')
-                <button class="btn btn-danger" type="submit">
-                    <!-- Logout icon -->
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-left" viewBox="0 0 16 16">
-                        <path fill-rule="evenodd" d="M6 12.5a.5.5 0 0 0 .5.5h8a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5h-8a.5.5 0 0 0-.5.5v2a.5.5 0 0 1-1 0v-2A1.5 1.5 0 0 1 6.5 2h8A1.5 1.5 0 0 1 16 3.5v9a1.5 1.5 0 0 1-1.5 1.5h-8A1.5 1.5 0 0 1 5 12.5v-2a.5.5 0 0 1 1 0z"/>
-                        <path fill-rule="evenodd" d="M.146 8.354a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L1.707 7.5H10.5a.5.5 0 0 1 0 1H1.707l2.147 2.146a.5.5 0 0 1-.708.708z"/>
-                    </svg>
-                    <!-- END ICON -->
+                <button class="btn btn-danger"  type="submit">
+                    
                 </button>
         </form>
     @endguest
