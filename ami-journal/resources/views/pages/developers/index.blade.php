61937a643340baa2b437d0fc4d4d189d804cbb96
lilkinczi <adam.kinczel@gmail.com>
2024-05-09 13:04:49 +0200

    Footer modify and Developers page

---
+++ ami-journal/resources/views/pages/developers/index.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.main')
+
+@section('content')
+<body style="background: rgb(255,255,255);border-bottom-width: 3px;border-bottom-style: none;">
+    <div class="container">
+        <h1>Our Team:</h1>
+        <p>Vámos Ákos János</p>
+        <p>Vámos Márton István</p>
+        <p>Bodnár Máté</p>
+        <p>Császár Martin Krisztián</p>
+        <p>Dávid Buda</p>
+        <p>Guba József</p>
+        <p>Gömöri Dániel</p>
+        <p>Juhász Gergő</p>
+        <p>Kakuk Márton</p>
+        <p>Kinczel Ádám Gergő</p>
+        <p>Mizere Robin Roland</p>
+        <p>Nagy Kristóf</p>
+        <p>Penyaskó Dávid</p>
+        <p>Sándor Dániel</p>
+        <p>Sánta András Attila</p>
+        <p>Tóth Tamás</p>
+        
+    </div>
+</body>
+@endsection
