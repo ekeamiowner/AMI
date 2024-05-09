f016bf63df42dab54654ac947fce208530fa8c41
vamosmarton <vammarton@gmail.com>
2024-05-09 14:08:36 +0200

    updated dev teams

--- ami-journal/resources/views/pages/developers/index.blade.php
+++ ami-journal/resources/views/pages/developers/index.blade.php
@@ -4,8 +4,9 @@
 <body style="background: rgb(255,255,255);border-bottom-width: 3px;border-bottom-style: none;">
     <div class="container">
         <h1>Our Team:</h1>
-        <p>Vámos Ákos János</p>
-        <p>Vámos Márton István</p>
+        
+        <p>Ács Róbert</p>
+        <P>Bagoly Gábor</P>
         <p>Bodnár Máté</p>
         <p>Császár Martin Krisztián</p>
         <p>Dávid Buda</p>
@@ -14,13 +15,14 @@
         <p>Juhász Gergő</p>
         <p>Kakuk Márton</p>
         <p>Kinczel Ádám Gergő</p>
+        <p>Kónya Donát</p>
         <p>Mizere Robin Roland</p>
         <p>Nagy Kristóf</p>
         <p>Penyaskó Dávid</p>
         <p>Sándor Dániel</p>
-        <p>Sánta András Attila</p>
-        <p>Tóth Tamás</p>
-        
+        <p>Vámos Ákos János</p>
+        <p>Vámos Márton István</p>
+        <p>Verebélyi Valentin</p>
     </div>
 </body>
 @endsection
